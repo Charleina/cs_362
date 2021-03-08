7c384455f0c411ab90f36fdef8da6795a19eef19
Charlene <wangchar@oregonstate.edu>
2021-03-07 23:25:19 -0800

    Code worked and test worked came up with ok (first commit it was actually an error)

--- hw7/Fizzbuzz/fizzbuzz.py
+++ hw7/Fizzbuzz/fizzbuzz.py
@@ -1,5 +1,16 @@
 class fizzbuzz:
     def __init__(self):
         pass
-    def Fizzbuzz(self):
-        pass
+    def method(self):
+        fullstring = ""
+        for i in range(1, 101):
+            if(i%5 == 0 and i%3 == 0):
+                fullstring = fullstring + "FizzBuzz "
+            else:
+                if(i%3 == 0):
+                    fullstring = fullstring + "Fizz "
+                if(i%5 == 0):
+                    fullstring = fullstring + "Buzz "
+                if(i%3 !=0 and i%5 !=0):
+                    fullstring = fullstring + str(i) + " "
+        return fullstring
