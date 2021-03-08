f764aa3db154ae4d2d1401b3a727b8f9b31b0afb
Charlene <wangchar@oregonstate.edu>
2021-03-07 23:37:19 -0800

    fixed mistake, Wrote first test, got false

--- hw7/leapyear/test_leapyear.py
+++ hw7/leapyear/test_leapyear.py
@@ -6,7 +6,7 @@ class TestCase(unittest.TestCase):
         self.leapyear = leapyear()
         
     def test1(self):
-        self.assertEqual(self.leapyear.leapyear(), "True")
+        self.assertEqual(self.leapyear.leapyear(2020), "True")
         
 if __name__ == '__main__':
     unittest.main(verbosity=2)
