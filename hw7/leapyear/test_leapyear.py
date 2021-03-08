3aa91bcf956b331b87e4047196a0f32905f5640d
Charlene <wangchar@oregonstate.edu>
2021-03-07 23:49:26 -0800

    added another test to leapyear to test a false one

--- hw7/leapyear/test_leapyear.py
+++ hw7/leapyear/test_leapyear.py
@@ -8,5 +8,8 @@ class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(self.leapyear.leapyear(2020), "True")
         
+    def test2(self):
+        self.assertEqual(self.leapyear.leapyear(2019), "false")
+        
 if __name__ == '__main__':
     unittest.main(verbosity=2)
